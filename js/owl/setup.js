cdf6e58a2c57453d68b0d64b51fedfba200d2c49
Pedro Vaz <pedrovazs@outlook.com>
2021-10-15 15:19:10 -0300

    adiciona responsividade

--- js/owl/setup.js
+++ js/owl/setup.js
@@ -1,16 +1,32 @@
-$('.owl-carousel').owlCarousel({
+// $('.owl-carousel').owlCarousel({
+//     loop:true,
+//     margin:10,
+//     nav:false,
+//     responsive:{
+//         0:{
+//             items:1
+//         },
+//         600:{
+//             items:3
+//         },
+//         1000:{
+//             items:5
+//         }
+//     }
+// })
+
+var owl = $('.owl-carousel');
+owl.owlCarousel({
+    items:4,
     loop:true,
     margin:10,
-    nav:false,
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:3
-        },
-        1000:{
-            items:5
-        }
-    }
+    autoplay:true,
+    autoplayTimeout:8000,
+    autoplayHoverPause:true
+});
+$('.play').on('click',function(){
+    owl.trigger('play.owl.autoplay',[1000])
+})
+$('.stop').on('click',function(){
+    owl.trigger('stop.owl.autoplay')
 })
